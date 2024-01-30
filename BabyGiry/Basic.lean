c510eb89d4938d8d43809583fe87a76a4ced749c
Benedikt Peterseim <bp@peterseim.net>
2024-01-30 18:33:21 +0100

    added coinflip example

--- BabyGiry/Basic.lean
+++ BabyGiry/Basic.lean
@@ -1,4 +1,5 @@
 import Mathlib.Data.Finset.Basic
+import Mathlib.Data.Rat.Basic
 
 open Finset
 
@@ -6,8 +7,13 @@ structure FinProb (α : Type) where
   supported_in : Finset α
   prob_mass_fun : { x // (supported_in : Set α) x } → ℚ
 
+def coinflip : FinProb Bool where
+  supported_in := {true, false}
+  prob_mass_fun := fun _ => 1/2
+
+
 -- TODO :
--- 1. write down example of a FinProb (e.g. coinflip)
+-- 1. write down example of a FinProb (e.g. coinflip) (DONE)
 -- 2. add requirements : positivity, sum to 1
 -- 3. adjust example
 -- 4. implement monad structure
