225bf3938090ec9381623b2b5a7a4ccf2b4dd171
Benedikt Peterseim <bp@peterseim.net>
2024-01-31 12:08:13 +0100

    added conditions and proof in coinflip example

--- BabyGiry/Basic.lean
+++ BabyGiry/Basic.lean
@@ -1,21 +1,33 @@
 import Mathlib.Data.Finset.Basic
 import Mathlib.Data.Rat.Basic
+import Mathlib.Algebra.BigOperators.Basic
+import Mathlib.Tactic
 
-open Finset
+open Finset BigOperators
 
 structure FinProb (α : Type) where
-  supported_in : Finset α
-  prob_mass_fun : { x // (supported_in : Set α) x } → ℚ
+  support : Finset α
+  prob_mass_fun : support → ℚ
+  nonnegative : ∀ x : support, prob_mass_fun x ≥ 0
+  normalized : ∑ x, prob_mass_fun x = 1
+
 
 def coinflip : FinProb Bool where
-  supported_in := {true, false}
+  support := {true, false}
   prob_mass_fun := fun _ => 1/2
+  nonnegative := by
+    intro
+    simp
+    norm_num
+  normalized := by rfl
 
 
 -- TODO :
 -- 1. write down example of a FinProb (e.g. coinflip) (DONE)
--- 2. add requirements : positivity, sum to 1
--- 3. adjust example
--- 4. implement monad structure
--- 5. introduce nice notation
--- 6. nice non-trivial example
+-- 2. add requirements : positivity, sum to 1 (DONE)
+-- 3. adjust example (DONE)
+-- 4. Write P : FinProb Bool → ℚ evaluting prob_mass_fun at true, rename prob_mass_fun to mass?
+--    Add some comments.
+-- 5. implement monad structure
+-- 6. introduce nice notation
+-- 7. nice non-trivial example
