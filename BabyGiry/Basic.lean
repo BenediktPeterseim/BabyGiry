53476a6e696f943d897b31ff54f27e38861e0a82
Benedikt Peterseim <bp@peterseim.net>
2024-01-31 17:20:15 +0100

    changed TODOs slightly

--- BabyGiry/Basic.lean
+++ BabyGiry/Basic.lean
@@ -27,7 +27,7 @@ def coinflip : FinProb Bool where
 -- 2. add requirements : positivity, sum to 1 (DONE)
 -- 3. adjust example (DONE)
 -- 4. Write P : FinProb Bool → ℚ evaluting prob_mass_fun at true, rename prob_mass_fun to mass?
---    Add some comments.
+--    Add some comments. Remove simp by using "simp?". Rename coinflip to BernoulliTrial?
 -- 5. implement monad structure
 -- 6. introduce nice notation
 -- 7. nice non-trivial example
